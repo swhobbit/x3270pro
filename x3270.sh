944f9e7349ad6a4f0a5b89044c9d6e574c2fc552
Drew Derbyshire <ahd@kew.com>
2020-05-07 11:05:29 -0700

    Don't force scrollbar on in x3270

--- x3270.sh
+++ x3270.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 column=`expr $RANDOM % 1700`
 row=`expr $RANDOM % 300`
-x3270 -activeicon  -sb -once -geometry +${column}+${row} "$@" &
+x3270 -activeicon -once -geometry +${column}+${row} "$@" &
